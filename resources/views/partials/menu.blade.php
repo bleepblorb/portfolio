be4631cbfa23862aac9f7f18c7d83cc842440144
Adam Volkman <adam.volkman@gmail.com>
2017-04-12 10:56:57 -0700

    Add main site menu

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,23 @@
+<div id="main-menu" class="py-2 -max--lgs">
+  <div class="container">
+    <div class="g__row -align-items--center">
+      <div class="g__col">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('home')}}"><span class="t__h5 c--lightning">well done</span></a>
+          </li>
+        </ul>
+      </div>
+      <div class="g__col">
+        <ul class="nav -justify-content--end">
+          <li class="nav-item">
+            <a href="{{route('resume')}}" class="nav-link active">Resume</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Visuals</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
