c8f1dd96b9f50b3ea8c653ea61d330648d123d5f
Adam Volkman <adam.volkman@gmail.com>
2017-06-07 16:49:13 -0700

    Add temp logo to menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,10 +1,12 @@
+
 <div id="main-menu" class="py-2 -max--lgs">
   <div class="container">
     <div class="g__row -align-items--center">
       <div class="g__col">
         <ul class="nav">
+          <li class="nav-item c--lightning"><icon class="edit-btn" name="logo__mark" style="width: 2rem"></icon></li>
           <li class="nav-item">
-            <a class="nav-link" href="{{route('home')}}"><span class="t__h5 c--lightning">well done</span></a>
+            <a class="t--h4 nav-link" href="{{route('home')}}"><span class="t__h5 c--gray-light">well done</span></a>
           </li>
         </ul>
       </div>
