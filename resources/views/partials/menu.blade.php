03b46eb55034f6148b35032af804abb5f0830b1e
Adam Volkman <adam.volkman@gmail.com>
2017-06-20 22:00:22 -0700

    Tweaks to menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,24 +1,13 @@
 
-{{-- <div class="c--lightning logo-mark"><icon class="edit-btn" name="logo__mark" style="width: 2rem"></icon><br><icon class="edit-btn c--gray-light logo-text" name="logo__text-3" style="width: 1.25rem"></icon></div> --}}
+<a href="/" class="logo">
+  <icon class="c--gray-lighter" name="logo__mark" style="width: 2rem"></icon>
+  <icon class="logo-text c--gray"  name="logo__text" style="width: 3.5rem"></icon>
+</a>
 
 <div id="main-menu" class="py-2">
 
   <div class="container">
-    <div class="g__row -align-items--center">
-      <div class="g__col">
-        <ul class="nav">
-          <li class="nav-item c--gray-lighter logo">
-            <icon name="logo__mark" style="width: 2rem"></icon>
-            <icon class="logo-text c--gray"  name="logo__text" style="width: 3.5rem"></icon>
-          </li>
-          {{-- <li class="nav-item">
-            <a class="t--h4 nav-link" href="{{route('home')}}"><span class="t--h5 c--gray-light">well done</span></a>
-          </li> --}}
-          {{-- <li class="nav-item logo-text">
-            <a class="t--h4 c--gray " href="{{route('home')}}"><icon  name="logo__text-2" style="width: 4.5rem"></icon></a>
-          </li> --}}
-        </ul>
-      </div>
+    <div class="g__row -align-items--e">
       <div class="g__col">
         <ul class="nav -justify-content--end">
           <li class="nav-item">
