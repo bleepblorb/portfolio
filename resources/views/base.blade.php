1e483b4f14e43e0fd696c3e28028bcea15898cd1
Adam Volkman <adam.volkman@gmail.com>
2017-08-18 16:48:41 -0700

    Add Google analytics / new ascii art

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -17,18 +17,22 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     @yield('head')
-    <!--
-*********************************************************************************************
-**                                                                                         **
-**   ,--.   ,--.  ,------.  ,--.    ,--.      ,------.    ,-----.   ,--.  ,--.   ,-----.   **
-**   |  |   |  |  |  .---'  |  |    |  |      |  ,--. \  |  ,-.  |  |   \ |  |  |  .---'   **
-**   |  |   |  |  |  '--.   |  |    |  |      |  |  |  | |  | |  |  |  . '|  |  |  '--.    **
-**   |  |.'.|  |  |  .--'   |  |    |  |      |  |  /  | |  | |  |  |  |\    |  |  .--'    **
-**   |   ,'.   |  |  `---.  |  `--. |  `---.  |  '-'  /  |  '-'  |  |  | \   |  |  `---.   **
-**   `--'   '--'  `------'  `-----'  `-----'   `-----'    `-----'   `--'  `--'  `------'   **
-**                                                                                         **
-*********************************************************************************************
--->
+    <!--                                   
+                   /¯­­¯\             
+                   |  /¯¯­­\          
+                   |  |  /¯­­¯\       
+                   |  |  |  |       
+            ,-­¯­¯­­¯­­-,|  |  |  |       
+           /  ,--.       |  |       
+          '__/    |         |       
+          |  |    |         |       
+           \  `--´           \      
+            `-____,           \     
+                   \           \    
+       well done    \      _--¯¯    
+                     \_--¯­­¯­­­         
+    -->
+  
   </head>
   <body>
     <div id="app" :class="{'-hide-menu' : !state.showMenu}" v-cloak >
@@ -36,6 +40,7 @@
       @yield('app')
     </div>
   </body>
+  
   <!-- Scripts -->
   <script>
     window.Laravel = {!! json_encode([
@@ -44,4 +49,16 @@
   </script>
   <script src="{{mix('js/app.js')}}"></script>
   @stack('scripts')
+
+  <!-- Google Analytics -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-33534200-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 </html>
