2154e84140f7d255838cb015144f8a415e4c933e
Adam Volkman <adam.volkman@gmail.com>
2017-08-20 21:44:21 -0700

    Remove google analytics from local site

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -17,22 +17,22 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     @yield('head')
-    <!--                                   
-                   /¯­­¯\             
-                   |  /¯¯­­\          
-                   |  |  /¯­­¯\       
-                   |  |  |  |       
-            ,-­¯­¯­­¯­­-,|  |  |  |       
-           /  ,--.       |  |       
-          '__/    |         |       
-          |  |    |         |       
-           \  `--´           \      
-            `-____,           \     
-                   \           \    
-       well done    \      _--¯¯    
-                     \_--¯­­¯­­­         
+    <!--
+                   /¯­­¯\
+                   |  /¯¯­­\
+                   |  |  /¯­­¯\
+                   |  |  |  |
+            ,-­¯­¯­­¯­­-,|  |  |  |
+           /  ,--.       |  |
+          '__/    |         |
+          |  |    |         |
+           \  `--´           \
+            `-____,           \
+                   \           \
+       well done    \      _--¯¯
+                     \_--¯­­¯­­­
     -->
-  
+
   </head>
   <body>
     <div id="app" :class="{'-hide-menu' : !state.showMenu}" v-cloak >
@@ -40,7 +40,7 @@
       @yield('app')
     </div>
   </body>
-  
+
   <!-- Scripts -->
   <script>
     window.Laravel = {!! json_encode([
@@ -50,15 +50,17 @@
   <script src="{{mix('js/app.js')}}"></script>
   @stack('scripts')
 
-  <!-- Google Analytics -->
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  @if ( env('APP_ENV') === 'production')
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-33534200-1', 'auto');
-    ga('send', 'pageview');
+      ga('create', 'UA-33534200-1', 'auto');
+      ga('send', 'pageview');
 
-  </script>
+    </script>
+  @endif
 </html>
